70ef4cf33de4f9d4276e97fa1330b7e209ce52ea
Robin Millette <robin@millette.info>
2023-09-27 20:02:37 -0400

    Cleanup.

--- index.js
+++ index.js
@@ -9,36 +9,18 @@ const db = new Database("woot.db")
 db.pragma('journal_mode = WAL')
 const insert = db.prepare('INSERT INTO woot (json) VALUES (?)');
 
-// insert.run(JSON.stringify({ a: "b" }))
+const fastify = Fastify({ logger: true })
 
-const fastify = Fastify({
-  logger: {
-    transport: {
-      target: 'pino-pretty',
-      /*
-      options: {
-        translateTime: 'HH:MM:ss Z',
-        ignore: 'pid,hostname',
-      },
-      */
-    },
-  },
-})
-
-fastify.log.info(process.env.ROUTE)
-
-// Declare a route
+// TODO: handle route in caddy instead
 fastify.post(`/${process.env.ROUTE}`, function (request, reply) {
   const b = insert.run(JSON.stringify({ body: request.body }))
   request.log.info(JSON.stringify(b))
   reply.send({ hello: 'world' })
 })
 
-// Run the server!
-fastify.listen({ port: 3123 }, function (err, address) {
+fastify.listen({ port: 3123 }, function (err) {
   if (err) {
     fastify.log.error(err)
     process.exit(1)
   }
-  // Server is now listening on ${address}
 })
